c0383d25512353404b61f793609e2fc4d374608d
Brian Kelley <bkelley620@gmail.com>
2016-03-05 17:28:46 -0500

    Fixed some shit and I'm now maintaining a list of all peers in the server

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ var sha256 = require('js-sha256');
 var peers = [];
 var page_hashes = {'/hello': sha256('hello')}
 
+var server = app.listen(9000);
+
+var options = {
+    debug: true
+}
+
+peerServer = ExpressPeerServer(server, options);
+
 app.get('/', function(req, res, next) { 
 
 
@@ -15,32 +23,36 @@ app.get('/', function(req, res, next) {
 
 });
 
+app.use('/api', peerServer);
+
 app.get('/*', function(req, res, next) { 
 	if (req.url in page_hashes) {
 		sendjson = {'peer_id': 'peer_id', 'content_hash': page_hashes[req.url]};
+		res.send(JSON.stringify(sendjson));
 	} else {
 		res.send('404 page not found');
 	}
 	
-	res.send(JSON.stringify(sendjson));
+	
 });
 
-var server = app.listen(9000);
-
-var options = {
-    debug: true
-}
-
-app.use('/api', ExpressPeerServer(server, options));
-
-
-
-server.on('connection', function(id) {
+peerServer.on('connection', function(id) {
 	// send content if no others fully updated, otherwise send a client to connect to
-	console.log("Connected!");
+	console.log("Connected! id: " + id);
+	peers.push(id);
+	console.log('Peers: ' + peers);
 });
 
-server.on('disconnect', function(id) { 
+peerServer.on('disconnect', function(id) { 
 	// Remove client from list
-	console.log("Disconnected!");
+
+	var index = peers.indexOf(id);
+
+	if (index > -1) {
+	    peers.splice(index, 1);
+	}
+
+	console.log("Disconnected! id: " + id + " index in array: " + index);
+	console.log('Peers: ' + peers);
+
 });
