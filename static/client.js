7eba5399745034fb0d0975e9eca2f987ac23bcbc
minstz <minstz@rpi.edu>
2016-03-06 08:15:46 -0500

    Simulated resource delay + cleanup

--- static/client.js
+++ static/client.js
@@ -30,18 +30,22 @@ function setUpClient(requestURL) {
 		//This means we are currently the only active connection on the server
 		//We will now get the content directly from server
 		if (peerID == id || peerID == "peer_id") {
-			$.ajax({
-				url: '/' + id + requestURL,
-				dataType: 'html',
-				async: false,
-				success: function(html) {
-					console.log("Got the html ", html);
-					document.documentElement.innerHTML = html;
-					console.log(sha256(html));
-					console.log(html.length);
-					// document.write(html);
-				}
-			});
+			setTimeout(function() {
+				$.ajax({
+					url: '/' + id + requestURL,
+					dataType: 'html',
+					async: false,
+					success: function(html) {
+						console.log("Got the html ", html);
+						document.documentElement.innerHTML = html;
+						console.log(sha256(html));
+						console.log(html.length);
+						// document.write(html);
+					}
+				});
+
+			},2000); //simulate resource delay
+
 		}
 
 		else {
