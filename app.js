a03e205decb49cf49ba21037639946b8600ef870
minstz <minstz@rpi.edu>
2016-03-06 08:47:12 -0500

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit e89e323d42e3333129f8ea1e9dab2d41c11d423b, reversing
    changes made to 7eba5399745034fb0d0975e9eca2f987ac23bcbc.

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.get('/*/*_metadata', function(req, res, next) {
 	if (url in page_hashes) {
 
 		// by default, tell them to get the content from themselves. This means get it from the server.
-		var to_connect_to = 'peer_id';
+		var to_connect_to = peer;
 
 		for (var i = 0; i < peers.length; i++) {
 			console.log(peers[i]['id']);
@@ -136,7 +136,6 @@ peerServer.on('disconnect', function(id) {
 	// Remove client from list
 	console.log("disconnected id: " + id);
 	remove_peer(id);
-	console.log("Disconnected!");
 
 });
 
